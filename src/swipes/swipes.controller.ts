9e26f2e5d683c89d3ae7acb3ef5cdc843ddcfba5
Subham Nayak <nayaksubham2003@gmail.com>
2025-10-22 17:33:45 +0530

    ckbd

--- src/swipes/swipes.controller.ts
+++ src/swipes/swipes.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, Req, UseGuards } from '@nestjs/common';
 import { SwipesService } from './swipes.service';
+import { CreateSwipeDto } from './dto/create-swipe.dto';
+import { SwipeFeedDto } from './dto/swipe-feed.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('swipes')
 export class SwipesController {
   constructor(private readonly swipesService: SwipesService) {}
+
+
+  @UseGuards(JwtAuthGuard)
+  @Get('feed')
+  async getFeed(@Req() req , @Query() dto: SwipeFeedDto){
+    const userID = req.user.sub;
+    return this.swipesService.swipefeed(userID , dto)
+  }
+
+  
+
+
+
 }
--- src/swipes/swipes.module.ts
+++ src/swipes/swipes.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SwipesService } from './swipes.service';
 import { SwipesController } from './swipes.controller';
+import {PrismaService} from '../prisma /prisma.service';
 
 @Module({
   controllers: [SwipesController],
-  providers: [SwipesService],
+  providers: [SwipesService , PrismaService],
 })
 export class SwipesModule {}
