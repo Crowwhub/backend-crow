8f8f9b443d2ea57530a98deaaae4a4b967f046f6
Subham Nayak <nayaksubham2003@gmail.com>
2025-10-26 02:24:57 +0530

    match controller added

--- src/matches/matches.controller.ts
+++ src/matches/matches.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Delete, Param , UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { MatchesService } from './matches.service';
 
 @Controller('matches')
 export class MatchesController {
   constructor(private readonly matchesService: MatchesService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':userId')
+  async getusermatches(@Param('userId') userId : string){
+    return this.matchesService.getUserMatches (userId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':matchId')
+  async deletematch (@Param('matchId') matchId : string){
+    return this.matchesService.deleteMatch (matchId);
+  }
+
 }
