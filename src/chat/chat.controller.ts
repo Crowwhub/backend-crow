9f10558ff7215bf62d9ad8d5ea9ecae7c8c0167b
Subham Nayak <nayaksubham2003@gmail.com>
2025-10-27 23:45:13 +0530

    chat controller added

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Delete, Param  } from '@nestjs/common';
 import { ChatService } from './chat.service';
 
 @Controller('chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
+
+async getChatHistory(@Param('matchId') matchId: string) {
+    return this.chatService.getMessages(matchId);
+  }
+
+  // DELETE /chat/message/:messageId -> delete a specific message
+  @Delete('message/:matchId')
+  async deleteMessage(@Param('matchId') matchId: string) {
+    return this.chatService.deleteMessage(matchId);
+  }
+
 }
