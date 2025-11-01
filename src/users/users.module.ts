3d222b55fc5e202fdbd5e648d74362cec23fa232
Subham Nayak <nayaksubham2003@gmail.com>
2025-11-02 00:27:06 +0530

    ty

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import {UserService} from './users.service';
+import { UserController } from './users.controller';
+import {PrismaService} from '../prisma /prisma.service';
+
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService],
+  controllers: [UserController],
+  providers: [UserService , PrismaService],
 })
 export class UsersModule {}
