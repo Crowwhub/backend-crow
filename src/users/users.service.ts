fe651ae5a42eedc455b29725af877581b3334018
Subham Nayak <nayaksubham2003@gmail.com>
2025-11-02 00:11:25 +0530

    user service added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma /prisma.service";
+import { UpdateDomainDto } from "./dto/update-domian.dto";
+import { UpdateSkillsDto } from "./dto/update-skills.dto";
+import { UpdateProfileDto } from "./dto/update-profile.dto";
+import { UpdatePurposeDto } from "./dto/update-purpose.dto";
 
 @Injectable()
-export class UsersService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async getMe(userId: string) {
+    return this.prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        username: true,
+        photo: true,
+
+        // onboarding fields
+        domain: true,
+        skills: true,
+        interests: true,
+        favouriteTools: true,
+        personType: true,
+        promptTagline: true,
+        madeTillFar: true,
+        purpose: true,
+        onboardingComplete: true,
+
+        createdAt: true,
+      },
+    });
+  }
+
+  async updateDomain(userId: string, dto: UpdateDomainDto) {
+    return this.prisma.user.update({
+      where: { id: userId },
+      data: { domain: dto.domain },
+    });
+  }
+
+  async updateSkills(userId: string, dto: UpdateSkillsDto) {
+    return this.prisma.user.update({
+      where: { id: userId },
+      data: { skills: dto.skills },
+    });
+  }
+
+  async updateProfile(userId: string, dto: UpdateProfileDto) {
+    return this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        name: dto.name,
+        personType: dto.personType,
+        interests: dto.interests,
+        favouriteTools: dto.favouriteTools,
+        promptTagline: dto.promptTagline,
+        madeTillFar: dto.madeTillFar,
+      },
+    });
+  }
+
+  async updatePurpose(userId: string, dto: UpdatePurposeDto) {
+    return this.prisma.user.update({
+      where: { id: userId },
+      data: { purpose: dto.purpose },
+    });
+  }
+
+  async completeOnboarding(userId: string) {
+    return this.prisma.user.update({
+      where: { id: userId },
+      data: { onboardingComplete: true },
+    });
+  }
+}
+
